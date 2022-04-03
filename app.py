a36f625b8ced4fa44f8015a907a442fe9bb5672b
Usman Zaheer <usmanozaheer@gmail.com>
2022-04-03 19:03:32 -0400

    User login authentication

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from passlib.hash import pbkdf2_sha256
 from wtform_fields import *
 from models import *
@@ -11,6 +12,13 @@ app.secret_key = 'MUST REPLACE'  #REPLACE SECRET KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qfdtdkunaroicj:c3d36f692efe24709974488ec30f5158d0b22cdf18eb470e854867cc2094f3bd@ec2-52-21-136-176.compute-1.amazonaws.com:5432/dcfco3jf2ii5bc'
 db = SQLAlchemy(app)
 
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader  
+def load_user(id):
+    return User.query.get(int(id))
+
 @app.route("/", methods=['GET', 'POST']) # Default route
 def index():
     user_reg_form = RegistrationForm() # instanciate class for form imported from our wtform_field.py
@@ -21,12 +29,14 @@ def index():
 
         hash_password = pbkdf2_sha256.hash(current_password)
         
-        current_user = User(username = current_username, password = hash_password)
-        db.session.add(current_user)
+        cur_user = User(username = current_username, password = hash_password)
+        db.session.add(cur_user)
         db.session.commit()
         return redirect(url_for('login'))
 
     return render_template("index.html", form = user_reg_form) # render the page
+
+
 
 # Route for login page
 @app.route("/login", methods=['GET', 'POST'])
@@ -34,9 +44,30 @@ def login():
     user_login_form = UserLoginForm()
     
     if user_login_form.validate_on_submit():
-        return "Successful Login"
+        cur_user = User.query.filter_by(username = user_login_form.username.data).first()
+        login_user(cur_user)
+        return redirect(url_for('chat'))
 
     return render_template("login.html", form = user_login_form)
 
-if __name__ == "__main__": # host server on local IP
+
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+    if not current_user.is_authenticated:
+        return "Must login before using chat"
+
+    return "Welcome to chat"
+
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    return "Successfully Logged Out"
+
+
+
+# host server on local IP
+if __name__ == "__main__":
     app.run(debug = True)
--- models.py
+++ models.py
@@ -1,13 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    """ User Profile """
-
+# User table/profile model
+class User(UserMixin, db.Model):
     __tablename__ = "user_table"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(25), unique = True, nullable = False)
     password = db.Column(db.String(), nullable = False)
-
-
